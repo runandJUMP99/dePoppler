7310ab67a8a7a07a20ddd532e43c7bf410e28fc6
Steven Velez <stevendvelez@yahoo.com>
2020-02-17 15:52:09 -0500

    Add new item and upload img modals

---
+++ public/index.js
@@ -0,0 +1,15 @@
+$(".item-img-container").on("click", function() {
+    $(".backdrop").addClass("open");
+    $(".modal__upload-img").addClass("open");
+});
+
+$(".upload-button").on("click", function() {
+    $(".backdrop").addClass("open");
+    $(".modal__new-item").addClass("open");
+});
+
+$(".backdrop").on("click", function() {
+    $(".backdrop").removeClass("open");
+    $(".modal__new-item").removeClass("open");
+    $(".modal__upload-img").removeClass("open");
+});
